e25671db127afe2b22893861928c3796f7bb5067
= <=>
2023-03-10 15:26:04 -0500

    Sorted linked list

---
+++ listicas/cmake-build-debug/CMakeFiles/listicas.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Dani/Desktop/Eafit/Lenguajes de programacion/Practica_1_ListasEnlazadas_WendyBenitez/listicas/Lista.cpp" "CMakeFiles/listicas.dir/Lista.cpp.obj" "gcc" "CMakeFiles/listicas.dir/Lista.cpp.obj.d"
+  "C:/Users/Dani/Desktop/Eafit/Lenguajes de programacion/Practica_1_ListasEnlazadas_WendyBenitez/listicas/Nodo.cpp" "CMakeFiles/listicas.dir/Nodo.cpp.obj" "gcc" "CMakeFiles/listicas.dir/Nodo.cpp.obj.d"
+  "C:/Users/Dani/Desktop/Eafit/Lenguajes de programacion/Practica_1_ListasEnlazadas_WendyBenitez/listicas/main.cpp" "CMakeFiles/listicas.dir/main.cpp.obj" "gcc" "CMakeFiles/listicas.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
